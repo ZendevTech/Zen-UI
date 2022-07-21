8900349040d97f4bfa0716708c4391b4df097a61
Owen Alexander <60371379+oalexander-dev@users.noreply.github.com>
2022-07-21 16:41:46 -0400

    Fixed button test

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -5,6 +5,8 @@ import Button from './Button';
 
 describe('Button', () => {
     test('renders the Button component', () => {
-        render(<Button>Hello world</Button>);
+        const result = render(<Button>Hello world</Button>).findByText('Hello world');
+
+        expect(result).toBeTruthy();
     });
 });
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,6 +2,10 @@ import React, { FC } from 'react';
 
 import { ButtonProps } from './Button.types';
 
+// bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded
+
+// bg-transparent hover:bg-slate-100 text-slate-900 font-semibold py-2 px-4 hover:border-transparent rounded
+
 const Button: FC<ButtonProps> = ({
     children,
     disabled = false,
